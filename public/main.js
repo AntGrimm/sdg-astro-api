240f85ded071e4559df8cc4c930ae31752e0e034
Tony Grimm <anthony.l.grimm@gmail.com>
2019-08-15 16:53:55 -0400

    added in API for main image

--- public/main.js
+++ public/main.js
@@ -1,7 +1,26 @@
+const createUrl = nasaSite => {
+  const API_URL = 'https://sdg-astro-api.herokuapp.com/api/Nasa/apod'
+  console.log(API_URL)
+  return API_URL
+}
+
+const getMainImage = async nasaSite => {
+  const response = await fetch(createUrl(nasaSite))
+  const nasaData = await response.json()
+
+  console.log(nasaData)
+  const mainImage = (document.querySelector(
+    '.image-area'
+  ).style.backgroundImage = `url(${nasaData.hdUrl})`)
+  console.log(nasaData.hdUrl)
+  document.querySelector('.image-copyright').textContent =
+    nasaData.copyright || 'no copyright'
+  document.querySelector('.image-title').textContent = nasaData.title
+}
+
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+  createUrl()
+  getMainImage()
 }
 
 document.addEventListener('DOMContentLoaded', main)
